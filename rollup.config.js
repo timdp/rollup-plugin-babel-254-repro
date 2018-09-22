2d885b65abc69779b6ad2bfc5d6adb5ca9f19e35
Tim De Pauw <timdp@users.noreply.github.com>
2018-09-22 17:31:10 +0200

    Illustrate influence of { babelrc: false }

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,24 @@
 import nodeResolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
+import pkg from './package.json'
+
+const { TARGET } = process.env
+
+const babelOptions = {
+  working: {},
+  failing: Object.assign({ babelrc: false }, pkg.babel)
+}[TARGET]
 
 export default {
   input: 'src/index.js',
   output: {
-    file: 'dist/index.js',
+    file: `dist/index.${TARGET}.js`,
     format: 'iife'
   },
   plugins: [
     nodeResolve(),
     commonjs(),
-    babel({
-      babelrc: false,
-      presets: [
-        ['@babel/preset-env', {
-          useBuiltIns: 'usage'
-        }]
-      ]
-    })
+    babel(babelOptions)
   ]
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-const sum = terms => terms.reduceRight((sum, term) => sum + term, 0)
+const sum1 = terms => terms.reduceRight((sum, term) => sum + term, 0)
 
-console.log(sum([1, 2, 3]))
+const sum2 = terms => {
+  let sum = 0
+  for (const term of terms) {
+    sum += term
+  }
+  return sum
+}
+
+console.log(sum1([1, 2, 3]))
+console.log(sum2([1, 2, 3]))
